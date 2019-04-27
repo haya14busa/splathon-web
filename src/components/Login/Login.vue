340804a0dbac681311318b719b2fc6b174ef515f
haya14busa <haya14busa@gmail.com>
2019-04-27 07:41:29 +0000

    Add skelton Login component

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="login">
+    <h2>Login</h2>
+  </div>
+</template>
+<style scoped lang="scss" src="./style.scss"></style>
+<script lang="ts" src="./script.ts"></script>
+
--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="admin">
     <p>This is an admin page</p>
+    <Login/>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Login from '@/components/Login/Login.vue';
+
+@Component({
+  components: {
+    Login,
+  },
+})
+export default class AdminHome extends Vue {}
+</script>
