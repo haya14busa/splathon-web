b2bdea61d88a8b6f697a55e50aa0e8fffd616aa6
haya14busa <haya14busa@gmail.com>
2019-04-27 10:44:23 +0000

    Call Login API

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="login">
     <h2>Login</h2>
+    <div>
+      <label for="id">ID</label>
+      <input v-model="id">
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input v-model="password" type="password">
+    </div>
+    <button @click="onSubmit">Login</button>
+    <p class="err">{{ errmsg }}</p>
   </div>
 </template>
 <style scoped lang="scss" src="./style.scss"></style>
--- src/components/Login/script.ts
+++ src/components/Login/script.ts
@@ -1,4 +1,32 @@
 import { Component, Vue } from 'vue-property-decorator';
+import * as api from '@/swagger/splathon-api/api';
+
 
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  private id: string = '';
+  private password: string = '';
+  private errmsg: string = '';
+
+  private defaultAPI = new api.DefaultApi();
+  private resultAPI = new api.ResultApi();
+
+  private onSubmit() {
+    this.errmsg = '';
+    const eventNumbering = 10; // Actually, any numbering is fine for admin login.
+    const req: api.LoginRequest = {
+      user_id: this.id,
+      password: this.password,
+    };
+    this.defaultAPI.login(eventNumbering, req).then((res: api.LoginResponse) => {
+      if (!res.is_admin) {
+        this.errmsg = 'Login failed';
+        return;
+      }
+    }).catch((err) => {
+      return err.json();
+    }).then((err: api.ModelError) => {
+      this.errmsg = err.message;
+    });
+  }
+}
