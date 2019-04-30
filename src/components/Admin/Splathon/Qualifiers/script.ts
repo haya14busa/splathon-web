bcbba9c4d366e43ae54de4a22f2d16759493313f
haya14busa <haya14busa@gmail.com>
2019-04-30 13:26:04 +0000

    Support updating released qualifier round

--- src/components/Admin/Splathon/Qualifiers/script.ts
+++ src/components/Admin/Splathon/Qualifiers/script.ts
@@ -6,4 +6,32 @@ import * as api from '@/swagger/splathon-api/api';
 export default class Qualifiers extends Vue {
   @Prop() private token!: string;
   @Prop() private eventNumbering!: number;
+
+  private releasedRound = -1;
+
+  protected created() {
+    AdminApi.getReleaseQualifier(this.eventNumbering, this.token)
+      .then((round: number) => {
+        this.releasedRound = round;
+      }).catch(this.handleErr);
+  }
+
+  private onUpdateReleaseRound() {
+    AdminApi.updateReleaseQualifier(this.eventNumbering, this.token, { round: this.releasedRound})
+      .then(() => {
+        // TODO(haya14busa): propagate reload method instead of reloading the whole page?
+        location.reload();
+      })
+      .catch(this.handleErr);
+  }
+
+  private handleErr(resp) {
+    if (resp.json) {
+      resp.json().then((err: api.ModelError) => {
+        console.log(err.message);
+      });
+    } else {
+      console.log(resp);
+    }
+  }
 }
